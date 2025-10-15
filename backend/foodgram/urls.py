62e962017f876b03e03b4ac7f46159605791152b
root <root@LAPTOP-ERS1M0A1.localdomain>
2025-10-15 15:40:58 +0300

    fix urls

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
 ]
