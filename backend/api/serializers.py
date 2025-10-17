f03a6ddcb98a02cef460f3611f544fceb354bf82
root <root@LAPTOP-ERS1M0A1.localdomain>
2025-10-17 13:10:02 +0300

    fix readme,serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,32 +1,20 @@
+import base64
+import uuid
+
 from django.db import transaction
 from rest_framework import serializers
-import base64
-import six
-import uuid
 from django.core.files.base import ContentFile
 
-from recipes.models import Tag, Ingredient, Recipe, RecipeIngredient
-from recipes.models import Favorite, ShoppingCart
+from recipes.models import Tag, Ingredient, Recipe, RecipeIngredient, Favorite, ShoppingCart
 
 class Base64ImageField(serializers.ImageField):
     def to_internal_value(self, data):
-        if isinstance(data, six.string_types):
-            if 'data:' in data and ';base64,' in data:
-                header, data = data.split(';base64,')
-            try:
-                decoded_file = base64.b64decode(data)
-            except TypeError:
-                self.fail('invalid_image')
-
-            file_name = str(uuid.uuid4())[:12]
-            file_extension = 'jpg'
-            if header.startswith('data:image/'):
-                file_extension = header.split('/')[1]
-            complete_file_name = "%s.%s" % (file_name, file_extension)
-
-            data = ContentFile(decoded_file, name=complete_file_name)
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            id = uuid.uuid4().hex[:10]
+            data = ContentFile(base64.b64decode(imgstr), name=f'{id}.{ext}')
         return super().to_internal_value(data)
-
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
@@ -38,50 +26,48 @@ class IngredientSerializer(serializers.ModelSerializer):
         model = Ingredient
         fields = ('id', 'name', 'measurement_unit')
 
-class RecipeIngredientSerializer(serializers.ModelSerializer):
-    id = serializers.PrimaryKeyRelatedField(
-        queryset=Ingredient.objects.all(),
-        source='ingredient'
-    )
-    name = serializers.CharField(
-        source='ingredient.name', read_only=True
-    )
-    measurement_unit = serializers.CharField(
-        source='ingredient.measurement_unit', read_only=True
-    )
+class RecipeIngredientReadSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(source='ingredient.id')
+    name = serializers.CharField(source='ingredient.name')
+    measurement_unit = serializers.CharField(source='ingredient.measurement_unit')
 
     class Meta:
         model = RecipeIngredient
         fields = ('id', 'name', 'measurement_unit', 'amount')
 
+class RecipeIngredientWriteSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    amount = serializers.IntegerField()
+
 class RecipeSerializer(serializers.ModelSerializer):
-    tags = serializers.PrimaryKeyRelatedField(
-        queryset=Tag.objects.all(), many=True
-    )
-    ingredients = RecipeIngredientSerializer(many=True)
-    author = serializers.ReadOnlyField(source='author.email')
-    image = Base64ImageField(required=True)  # !!! ВОТ ТУТ !!!
+    tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(), many=True)
+    ingredients = RecipeIngredientWriteSerializer(many=True)
+    author = serializers.SerializerMethodField()
+    image = Base64ImageField(required=True)
 
     class Meta:
         model = Recipe
         fields = (
-            'id', 'author', 'name', 'image', 'text', 'ingredients', 'tags',
-            'cooking_time', 'pub_date'
+            'id', 'author', 'name', 'image', 'text',
+            'ingredients', 'tags', 'cooking_time', 'pub_date'
         )
+
+    def get_author(self, obj):
+        user = obj.author
+        return {
+            "id": user.id,
+            "email": user.email,
+            "username": user.username,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+        }
 
     def to_representation(self, instance):
         rep = super().to_representation(instance)
         rep['tags'] = TagSerializer(instance.tags.all(), many=True).data
-        rep['ingredients'] = RecipeIngredientSerializer(
+        rep['ingredients'] = RecipeIngredientReadSerializer(
             instance.recipeingredient_set.all(), many=True
         ).data
-        rep['author'] = {
-            "id": instance.author.id,
-            "email": instance.author.email,
-            "username": instance.author.username,
-            "first_name": instance.author.first_name,
-            "last_name": instance.author.last_name
-        }
         return rep
 
     @transaction.atomic
@@ -90,12 +76,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         ingredients_data = validated_data.pop('ingredients')
         recipe = Recipe.objects.create(**validated_data)
         recipe.tags.set(tags)
-        for ingredient_data in ingredients_data:
-            RecipeIngredient.objects.create(
-                recipe=recipe,
-                ingredient=ingredient_data['ingredient'],
-                amount=ingredient_data['amount']
-            )
+        self._create_ingredients(recipe, ingredients_data)
         return recipe
 
     @transaction.atomic
@@ -108,14 +89,17 @@ class RecipeSerializer(serializers.ModelSerializer):
             instance.tags.set(tags)
         if ingredients_data is not None:
             instance.recipeingredient_set.all().delete()
-            for ingredient_data in ingredients_data:
-                RecipeIngredient.objects.create(
-                    recipe=instance,
-                    ingredient=ingredient_data['ingredient'],
-                    amount=ingredient_data['amount']
-                )
+            self._create_ingredients(instance, ingredients_data)
         instance.save()
         return instance
+
+    def _create_ingredients(self, recipe, ingredients_data):
+        for ingredient in ingredients_data:
+            RecipeIngredient.objects.create(
+                recipe=recipe,
+                ingredient=Ingredient.objects.get(id=ingredient['id']),
+                amount=ingredient['amount']
+            )
 
 class ShortRecipeSerializer(serializers.ModelSerializer):
     class Meta:
